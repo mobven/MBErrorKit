d6b898dc478239edb3fe25da1b36de113211f5c6
Rashidium <rashid.ramazanov@gmail.com>
2020-06-16 16:01:12 +0300

    Error Kit delegation.

--- Sources/ErrorKit/ErrorKit.swift
+++ Sources/ErrorKit/ErrorKit.swift
@@ -18,10 +18,28 @@ extension ErrorKit: MBComponent {
     
 }
 
+/// Delegation for `ErrorKit` errors.
+public protocol ErrorKitDelegate: class {
+    /// `ErrorKit` caught a networking error.
+    /// - parameter networkingError: Networking error.
+    func errorKitDidCatch(networkingError: Error)
+    
+    /// `ErrorKit` caught a JSON (de-)serialization error.
+    /// - parameter serializationError: Serialization error.
+    func errorKitDidCatch(serializationError: Error)
+    
+    /// `ErrorKit` caught an encryption/decryption error.
+    /// - parameter cryptographyError: Encryption/decryption error.
+    func errorKitDidCatch(cryptographyError: Error)
+}
+
 /// Manager for ErrorKit.
 public class ErrorKit {
     
     /// `ErrorKit` singleton instance.
     public static let shared: ErrorKit = ErrorKit()
+    
+    /// Delegation for errors.
+    public weak var delegate: ErrorKitDelegate?
     
 }
